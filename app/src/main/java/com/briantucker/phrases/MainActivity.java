d191859e12d128a363c8b83d3df7588a223ea0ea
Brain Tucker <brian.briantucker@gmail.com>
2018-02-02 11:52:44 -0600

    getting the id of the view being pressed and logging it in console

--- app/src/main/java/com/briantucker/phrases/MainActivity.java
+++ app/src/main/java/com/briantucker/phrases/MainActivity.java
@@ -2,8 +2,21 @@ package com.briantucker.phrases;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void buttonTapped(View view){
+
+        int id = view.getId();
+        String thisId = "";
+
+        thisId = view.getResources().getResourceEntryName(id);
+
+        Log.i("button tapped", thisId);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
